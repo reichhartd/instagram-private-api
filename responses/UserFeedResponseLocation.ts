4934c2680c8fb9d0532c855abd1779858018cfee
Daniel Reichhart <daniel.reichhart@gmx.net>
2021-10-19 23:43:04 +0200

    fix: Example for UserFeedResponseLocation

---
+++ responses/UserFeedResponseLocation.ts
@@ -0,0 +1,13 @@
+import { UserFeedResponseLocation } from '../src';
+
+export const positiveResponse: UserFeedResponseLocation = {
+  pk: 346308525947095,
+  short_name: 'Ingolstadt Clothing',
+  facebook_places_id: 346308525947095,
+  external_source: 'facebook_places',
+  name: 'Ingolstadt Clothing',
+  address: '',
+  city: '',
+  has_viewer_saved: null,
+  is_eligible_for_guides: true,
+};
